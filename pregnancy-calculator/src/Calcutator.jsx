a34050a2e53214efe679c633fd85c890d1afcff3
Katieum <eaglenyoni@gmail.com>
2025-11-02 02:33:25 +0200

    ES6 getting values dynamically

--- pregnancy-calculator/src/Calcutator.jsx
+++ pregnancy-calculator/src/Calcutator.jsx
@@ -8,20 +8,18 @@ export default function Calculator() {
     const [sex, setSex] = useState('');
     const [results, setResults] = useState('');
 
-    period = document.getElementById('period-date').value;
-    sex = document.getElementById('sex-date').value;
-
     const period_date = new Date(period);
     const sex_date = new Date(sex);
 
     
+    /*
     const ovulation_date_Lowerbound = new Date(period_date);
     const ovulation_date_Upperbound = new Date(period_date);
     ovulation_date_Lowerbound.setDate(ovulation_date_Lowerbound.getDate() + 12);
     ovulation_date_Upperbound.setDate(ovulation_date_Upperbound.getDate() + 14);
 
     const fertility_window_start = new Date(period);
-    fertility_window_start.setDate(fertility_window_start.getDate() + 7); /* the 8th day after period start | grey area */
+    fertility_window_start.setDate(fertility_window_start.getDate() + 7); // the 8th day after period start | grey area 
 
     if (sex_date >= period_date && sex_date < fertility_window_start) {
         results = "LOW";
@@ -32,6 +30,7 @@ export default function Calculator() {
     } else {
         results = "VERY LOW";
     }
+    */
 
     return(
         <>
@@ -40,10 +39,10 @@ export default function Calculator() {
                 <span className="content">
                     <h1>Pregnancy Calculator</h1>
                     <label htmlFor="period-date">1st Period Date</label>
-                    <input type="date" id="period-date" name="period-date" required />
+                    <input type="date" id="period-date" name="period-date" value={period} onChange={e => setPeriod(e.target.value)} required />
                     <br />
                     <label htmlFor="sex-date">Sex Date</label>
-                    <input type="date" id="sex-date" name="sex-date" required />
+                    <input type="date" id="sex-date" name="sex-date" value={sex} onChange={e => setSex(e.target.value)} required />
                     <br />
                     <p>Are you pregnant:</p>
                     <button type='submit' >Find Out</button>
