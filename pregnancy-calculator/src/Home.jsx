7dde7979efa0433c93fa80e8e4030c730f985311
Katieum <eaglenyoni@gmail.com>
2025-11-03 20:40:56 +0200

    File creation

---
+++ pregnancy-calculator/src/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import { CreateBrowserRouter, RouterProvider } from "react-router-dom";
+
+// install react-router-dom
+
+export default function Home() {
+    return (
+        <>
+            <h1>Home Page</h1>
+
+            
+        </>
+    );
+}
